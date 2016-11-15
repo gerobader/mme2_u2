13c37d0412d6ee69c5829deb7f54eccaf90a301b
jonathankofahl <s59275@beuth-hochschule.de>
2016-11-15 13:07:49 +0100

    merge

--- uebung003/app.js
+++ uebung003/app.js
@@ -4,7 +4,7 @@
  * On each restart the db will be reset (it is only in memory).
  * Best start with GET http://localhost:3000/tweets to see the JSON for it
  *
- * TODO: Start the server and play a little with Postman
+ * TODO: Start the server and play a little with Postman  -> Done
  * TODO: Look at the Routes-section (starting line 68) and start there to add your code 
  * 
  * @author Johannes Konert
