09613b90089c81d146596ee5072666577d2c0a95
jonathankofahl <s59275@beuth-hochschule.de>
2016-11-29 13:47:33 +0100

    uebung004 weit vorangebracht. bin stolz auf uns

--- uebung004/routes/videos.js
+++ uebung004/routes/videos.js
@@ -19,7 +19,6 @@ var store = require('../blackbox/store');
 
 var videos = express.Router();
 
-
 // if you like, you can use this for task 1.b:
 var requiredKeys = {title: 'string', src: 'string', length: 'number'};
 var optionalKeys = {description: 'string', playcount: 'number', ranking: 'number'};
@@ -27,21 +26,101 @@ var internalKeys = {id: 'number', timestamp: 'number'};
 
 
 // routes **********************
+
+
 videos.route('/')
-    .get(function(req, res, next) {
-        // TODO
-
-
-
-        next();
-    })
-    .post(function(req,res,next) {
-        // TODO
+    .get(function(req,res,next) {
+        res.locals.items = store.select('videos');
         next();
     });
 
 
-// TODO
+videos.route('/:id')
+    .get(function(req, res, next) {
+        res.json(store.select('videos',req.params.id));
+        next();
+    })
+
+    .put(function(req,res,next) {
+    // TODO
+        var err = undefined;
+        if(req.params.id == req.body.id){
+            if(req.body.title == undefined || req.body.src == undefined || req.body.length == undefined || req.body.length < 0){
+                err = new Error('required parameters are missing dude');
+                res.status(400).json(
+                    {
+                        "error": {
+                            "message": "at least one required parameter not set, required parameters are: title, source, length",
+                            "code" : 400
+                            }
+                    }
+                );
+                next(err);
+            }
+            if(req.body.description == undefined){
+                req.body.description = "";
+            }
+            if(req.body.playcount == undefined || req.body.playcount < 0){
+                req.body.playcount = 0;
+            }
+            if(req.body.ranking == undefined || req.body.ranking < 0){
+                req.body.ranking = 0;
+            }
+
+            store.replace('videos',req.params.id, req.body);
+            res.status(200).json(store.select('videos', req.body));
+            next();
+        } else {
+            err = new Error('can not replace dude');
+            err.status = 400;
+            next(err);
+        }
+    })
+
+    .delete(function(req, res, next) {
+        store.remove('videos', req.params.id);
+        res.status(200).end();
+        next();
+    })
+    .post(function(req, res, next) {
+        var err = undefined;
+        if(req.params.id == req.body.id){
+            if(req.body.title == undefined || req.body.src == undefined || req.body.length == undefined || req.body.length < 0){
+                err = new Error('required parameters are missing dude');
+                res.status(400).json(
+                    {
+                        "error": {
+                            "message": "at least one required parameter not set, required parameters are: title, source, length",
+                            "code" : 400
+                        }
+                    }
+                );
+                next(err);
+            }
+            if(req.body.description == undefined){
+                req.body.description = "";
+            }
+            if(req.body.playcount == undefined || req.body.playcount < 0){
+                req.body.playcount = 0;
+            }
+            if(req.body.ranking == undefined || req.body.ranking < 0){
+                req.body.ranking = 0;
+            }
+
+            var id = store.insert('videos',req.body);
+            res.status(201).json(store.select('videos', id));
+            next();
+        } else {
+            err = new Error('can not insert dude');
+            err.status = 400;
+            next(err);
+        }
+
+    });
+
+
+
+
 
 
 
